af3803bf297674220d5c9cb4c37252e32c6cdf20
Rakdav <rakdav@inbox.ru>
2023-03-18 12:34:47 +0200

    18.03.2023_4

--- lab678/MainWindow.xaml.cs
+++ lab678/MainWindow.xaml.cs
@@ -28,15 +28,50 @@ namespace lab678
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string str=txtInterMass.Text;
-            string[] strmas=str.Split(',');
-            string res = "";
-            foreach (var item in strmas)
+            if (txtInterMass.Text.Length != 0)
             {
-                mas.Add(int.Parse(item));
-                res += item+" ";
+                string str = txtInterMass.Text;
+                if (str[str.Length-1]==',') str=str.Substring(0,str.Length-1);
+                string[] strmas = str.Split(',');
+                string res = "";
+                foreach (var item in strmas)
+                {
+                    mas.Add(int.Parse(item));
+                    res += item + " ";
+                }
+                tbMassive.Text = res;
             }
-            tbMassive.Text = res;
+            else
+            {
+                MessageBox.Show("Введите элементы массива", "Сообщение",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int P = 1;
+            foreach (var item in mas)
+            {
+                if (item >= 2 && item < 8) P *= item;
+            }
+            tbResult.Text ="Прозведение элементов от [2,8) равно:"+P.ToString();
+        }
+
+        private void txtInterMass_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key >= Key.D0 && e.Key <= Key.D9 || e.Key == Key.OemComma||
+                e.Key==Key.Back||e.Key==Key.OemMinus)
+                return;
+            e.Handled = true;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            mas.Clear();
+            txtInterMass.Clear();
+            tbMassive.Text = "";
+            tbResult.Text = "";
         }
     }
 }
